001ff36153a4cd3250190b65f643a133a2ddc14b
Kusal Salpura <67365263+kusal2002@users.noreply.github.com>
2026-09-22 12:10:42 +0530

    Add energy transfer model and Mongo service

---
+++ SolarGridX/Services/EnergyTransferService.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Driver;
+using SolarGridX.Models;
+
+namespace SolarGridX.Services
+{
+    public class EnergyTransferService
+    {
+        private readonly IMongoCollection<EnergyTransfer> _transfers;
+
+        public EnergyTransferService(IMongoDatabase database)
+        {
+            _transfers = database.GetCollection<EnergyTransfer>(
+                "EnergyTransfers"
+            ); 
+        }
+
+        //Get All Energy Transfers
+        public async Task<List<EnergyTransfer>> GetAllAsync()
+        {
+            return await _transfers
+                .Find(_ => true)
+                .ToListAsync();
+        }
+
+        //Get A Energy Transfer By Id
+        public async Task<EnergyTransfer?> GetByIdAsync(string id)
+        {
+            return await _transfers
+                .Find(t => t.id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        //Create A New Energy Transfer
+        public async Task CreateAsync(EnergyTransfer transfer)
+        {
+            await _transfers.InsertOneAsync(transfer);
+        }
+    }
+}
