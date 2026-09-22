a6ccdcb4f2cf3eb9e5920e58265dfb3011a8fdf8
Kusal Salpura <67365263+kusal2002@users.noreply.github.com>
2026-09-22 10:57:31 +0530

    Revert "Add MongoDB setup"
    This reverts commit a3eb08aa48cd9ab6c5860774b75c009abb8dc15a.

--- SolarGridX/Settings/MongoDbSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SolarGridX.Settings
-{
-    public class MongoDbSettings
-    {
-        public string ConnectionString { get; set; } = string.Empty;
-
-        public string DatabaseName { get; set; } = string.Empty;
-    }
-}
